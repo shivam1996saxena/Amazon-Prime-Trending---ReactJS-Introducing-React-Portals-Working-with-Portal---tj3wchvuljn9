dd57bfd69741f7969a8c2f075aa252a92907f766
shivam1996saxena <110617996+shivam1996saxena@users.noreply.github.com>
2023-01-17 19:53:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import slides from './data.js';
-
-// ReactDOM.render(<App slides={slides}/>, document.getElementById('root'));
+ReactDOM.render(<App slides={slides}/>, document.getElementById('root'));
